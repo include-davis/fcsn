dd44ddaa4dfdc6e261330e3c46ccf46ee5389a26
Benjamin Dinh <bhdinh@ucdavis.edu>
2020-12-06 22:45:55 -0800

    done

--- fcsn.py
+++ fcsn.py
@@ -1,9 +1,4 @@
-import os
 from events_page import app
 
-# if __name__ == '__main__':
-#     app.run(debug=True, port=33507) # Allows 'python flaskblog.py' to be run flask with debugger continuously
-
-port = int(os.environ.get('PORT', 5000)) 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, port=33507) # Allows 'python flaskblog.py' to be run flask with debugger continuously
