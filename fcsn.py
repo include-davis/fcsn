1001d73ca95abe446391167c52cd73d7b6c9c556
Benjamin Dinh <bhdinh@ucdavis.edu>
2020-12-25 15:44:06 -0800

    done

--- fcsn.py
+++ fcsn.py
@@ -1,4 +1,4 @@
-from events_page import app
+from website import app
 
 if __name__ == '__main__':
     app.run(debug=True, port=33507) # Allows 'python flaskblog.py' to be run flask with debugger continuously
